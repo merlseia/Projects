9dd5cb1a3fff74bc52918a60bdef159fa70a455c
merlseia <emine.blbl0661@gmail.com>
2023-10-01 14:49:04 +0300

    Create p_cl.py

---
+++ p_cl.py
@@ -0,0 +1,23 @@
+from tkinter import*
+from tkinter.ttk import*
+
+from time import strftime
+
+root = Tk()
+root.title("Emos's Clock")
+
+
+def time():
+    stri= strftime('%H:%M:%S %p')
+    lbl.config(text=stri)
+    lbl.after(1000, time)
+
+lbl= Label(root, font=("calibri", 40, "bold"),
+            background= "pink",
+            foreground= "white")
+
+lbl.pack(anchor="center")
+
+
+time()
+mainloop()
